bcef76e15c9eb1922b66aab7e6fa4547902f519a
gla0450-ayushrawat <ayushrawat200511@gmail.com>
2026-03-09 11:08:48 +0530

    Add:UC8

--- QuantityMeasurmentApp.Tests/obj/Debug/net10.0/QuantityMeasurmentApp.Tests.AssemblyInfo.cs
+++ QuantityMeasurmentApp.Tests/obj/Debug/net10.0/QuantityMeasurmentApp.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuantityMeasurmentApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d368a99cb9876a8d4169b409e3652782465ca344")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e13ffdb89069f424624d451c663c91eaa9da7a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuantityMeasurmentApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuantityMeasurmentApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
